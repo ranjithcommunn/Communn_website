ee1ecaa9bc57d6faf704a0648bba2630bda4495b
Ranjith <ranjith@webxinfinity.com>
2025-01-18 23:33:25 +0530

    t

--- src/Components/Seo.tsx
+++ src/Components/Seo.tsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 // import { Helmet } from "react-helmet-async";
-import { Helmet } from "react-helmet";
+// import { Helmet } from "react-helmet";
 
 interface SEOProps {
   title: string;
@@ -8,29 +8,30 @@ interface SEOProps {
 
 const SEO: React.FC<SEOProps> = ({ title }) => {
   return (
-    <Helmet>
-      <meta charSet="utf-8" />
-      <title>{title}</title>
-      <meta
-        name="description"
-        content="Automate Community Management with AI-Powered
-          Subscription effortlessly
-          Our AI-powered platform enhances community management with tools for user data management, interaction,communication, subscription and payment processing, content management, and advanced analytics"
-      />
-      <meta name="format-detection" content="telephone=no" />
-      <meta
-        name="author"
-        content="Automate Community Management with AI-Powered
-          Subscription effortlessly
-          Our AI-powered platform enhances community management with tools for user data management, interaction,communication, subscription and payment processing, content management, and advanced analytics"
-      />
-      <meta
-        name="keywords"
-        content="Automate Community Management with AI-Powered
-          Subscription effortlessly
-          Our AI-powered platform enhances community management with tools for user data management, interaction,communication, subscription and payment processing, content management, and advanced analytics"
-      />
-    </Helmet>
+    // <Helmet>
+    //   <meta charSet="utf-8" />
+    //   <title>{title}</title>
+    //   <meta
+    //     name="description"
+    //     content="Automate Community Management with AI-Powered
+    //       Subscription effortlessly
+    //       Our AI-powered platform enhances community management with tools for user data management, interaction,communication, subscription and payment processing, content management, and advanced analytics"
+    //   />
+    //   <meta name="format-detection" content="telephone=no" />
+    //   <meta
+    //     name="author"
+    //     content="Automate Community Management with AI-Powered
+    //       Subscription effortlessly
+    //       Our AI-powered platform enhances community management with tools for user data management, interaction,communication, subscription and payment processing, content management, and advanced analytics"
+    //   />
+    //   <meta
+    //     name="keywords"
+    //     content="Automate Community Management with AI-Powered
+    //       Subscription effortlessly
+    //       Our AI-powered platform enhances community management with tools for user data management, interaction,communication, subscription and payment processing, content management, and advanced analytics"
+    //   />
+    // </Helmet>
+    <h1>{title}</h1>
   );
 };
 
