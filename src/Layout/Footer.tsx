95778ea7fc614600cce253269805c88731f4a037
Ranjith <ranjith.r@communn.io>
2025-05-29 16:55:00 +0530

    Updated Footer

--- src/Layout/Footer.tsx
+++ src/Layout/Footer.tsx
@@ -517,7 +517,7 @@ const Footer = () => {
                 }}
               >
                 NO 1/A MKK ROAD NEAR HARISCHANDRA GHAT MARRIYAPPAN PALYA RAJAJINAGAR 2ND STAGE , Bangalore North, Karnataka, India - 560021
-                CIN: U72900KA2020PTC142176
+                {/* CIN: U72900KA2020PTC142176 */}
               </Typography>
               <Typography
                 sx={{
